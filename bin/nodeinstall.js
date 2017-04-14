726f354023b989348939e3b064af71229c7bf60e
Jackson Tian <shyvo1987@gmail.com>
2017-04-14 16:04:46 +0800

    Use async await

--- bin/nodeinstall.js
+++ bin/nodeinstall.js
@@ -2,11 +2,9 @@
 
 'use strict';
 
-const co = require('co');
 const install = require('../lib/install');
 const program = require('commander');
 const only = require('only');
-
 
 program
   .version(require('../package.json').version)
@@ -26,19 +24,18 @@ if (!(program.args[0] || program.installNightly)) {
   process.exit(1);
 }
 
-co(function* () {
-  const options = only(program, [
-    'installNode',
-    'installNoderc',
-    'installAlinode',
-    'installNsolid',
-    'installNightly',
-    'china',
-    'cache',
-    'distUrl',
-  ]);
-  options.version = program.args[0];
-  yield install(options);
-}).catch(err => {
+const options = only(program, [
+  'installNode',
+  'installNoderc',
+  'installAlinode',
+  'installNsolid',
+  'installNightly',
+  'china',
+  'cache',
+  'distUrl',
+]);
+
+options.version = program.args[0];
+install(options).catch(err => {
   console.error(err.stack);
 });
